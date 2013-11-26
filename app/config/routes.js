b70981c0e48f0efb33611d8e9ec4f06cefdaad46
Brandon John-Freso <brandonjf@euclidtechnology.com>
2013-11-26 15:35:48 -0500

    Hooked up new routing for controllers and views

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,8 +1,8 @@
 //Some example configurations of views in angular with the route provider.
 eav.config(['$routeProvider', '$compileProvider', function($routeProvider) {
   $routeProvider.
-      when('/', {templateUrl: 'app/views/actionItems.htm', controller:'Controller1'}).
-      when('/view1/:someURLParam', {templateUrl: 'app/views/view2.htm', controller: 'Controller1'}).
+      when('/', {templateUrl: 'app/views/actionItems.htm', controller:'AiController'}).
+      when('/ai/:aiNumber', {templateUrl: 'app/views/actionItems.htm', controller: 'AiController'}).
       otherwise({redirectTo: 'app/views/view1.htm'});
 }])
 ;
