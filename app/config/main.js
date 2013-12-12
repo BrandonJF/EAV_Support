afb9a01ac22ed785b36aaa7e37cfded75ed2310a
Brandon John-Freso <brandonjf@euclidtechnology.com>
2013-12-12 10:49:06 -0500

    Integration of AI Priority Rendering

--- app/config/main.js
+++ app/config/main.js
@@ -3,10 +3,21 @@
  *
  * The module for eav
  */
-var eav = angular.module('eav', ['LocalStorageModule', 'ngRoute']).value('$anchorScroll', angular.noop);
+var eav = angular.module('eav', ['LocalStorageModule', 'ui.unique', 'ngRoute']).value('$anchorScroll', angular.noop);
 var config ={};
-config["username"] = "BJOHNFRESO";
-//alert("test");
+config["username"] = "bjohnfreso";
 
+
+
+
+
+function sumOfArray(theArray){
+    var sum =  _.reduce(theArray, function(prev, next) { return prev + next; }, 0);
+    console.log("your sum is", sum)
+}
+
+
+
+//calcProgress(testItems.actionItems);
 
 
