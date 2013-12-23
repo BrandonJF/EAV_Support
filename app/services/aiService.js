b766a3e74e95b0438e381765c80812d45681b362
Brandon John-Freso <brandonjf@euclidtechnology.com>
2013-12-23 18:28:48 -0500

    Extracted communication functions of AiService to create a new service, MessageService.

--- app/services/aiService.js
+++ app/services/aiService.js
@@ -49,52 +49,7 @@ eav.factory("aiService", function($http, localStorageService, userService) {
             });
         },
 
-        sendAiMessage: function(message, aiNumber) {
-            //alert(message, aiNumber);
-            var today = new Date();
-            return $http({
-                url: config.baseurl + '/eav/cgi-bin/actionitemsdll.dll/Info?',
-                method: 'get',
-                params: {
-                    "listitemnum": aiNumber,
-                    "WMT": "none",
-                    "WRP": "actionNote.htm",
-                }
-            }).then(function(response) {
-                var responseStatus = "";
-                var newnote = '<table width="100%" border="0"><tr><td bgcolor="#999999"><font color="white">Detailed Description Entered by ' + config.username + ' <BR>Time: ' + today.defaultView() + ' ' + window.getClockTime() + '</font></td></tr></table><p>' + message + '</p>';
-                var messageToSend = newnote + '<br>' + response.data;
-                var uploadPromise = aiService.uploadAiMessage(messageToSend, aiNumber);
-                uploadPromise.then(function(response) {
-                    responseStatus = response;
-                    return responseStatus;
-                });
-                return uploadPromise;
-            });
-        },
 
-        uploadAiMessage: function(message, aiNumber) {
-            console.log("Upload Ai Message: ", message);
-            var promise = $http({
-                //url: config.baseurl + '/eav/cgi-bin/actionitemsdll.dll/Info?',
-                url: config.baseurl + '/eav/cgi-bin/msashelpdll.dll/ActionItemUpdate',
-                method: 'get',
-                params: {
-                    "CUSTOMERCD": "502508",
-                    "listitemnum": aiNumber,
-                    "WMT": "none",
-                    "ACTIONNOTE": message,
-                    "SENDEMAIL": "N",
-                    "WRP": "UploadResponse.htm"
-                    //"ACTIONTYPE_field":"Client Review"
-
-                }
-            }).then(function(response) {
-                return response.data;
-            });
-            return promise;
-
-        },
 
         getBookmarks: function() {
             //console.log("getBookmarks Called.");
